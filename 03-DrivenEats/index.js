00aa179550b4a73abe7f2eed75b20fe1beec7146
pedro imbrech <elprogramadorpedro@gmail.com>
2022-01-04 15:57:19 -0300

    Seleção de itens

--- 03-DrivenEats/index.js
+++ 03-DrivenEats/index.js
@@ -1,9 +1,44 @@
-document.querySelector("#box-eats")
-    .addEventListener("wheel", event => {
-        if (event.deltaY > 0) {
-            event.target.scrollBy(300, 0)
-        } else {
-            event.target.scrollBy(-300, 0)
-        }
-    })
+let sum = 0;
+let orderchoice;
+let valorfood;
+let priceorder;
 
+function selectionfood(produtosx) {
+  const selectionold = document.querySelector(".fooda .border");
+  console.log(produtosx);
+  if (selectionold !== null) {
+    selectionold.classList.remove("border");
+    sum = sum - 1;
+  }
+  const nselect = document.querySelector(produtosx);
+  console.log(nselect);
+  nselect.classList.add("border");
+  sum = sum + 1;
+}
+
+function selectiondrink(produtosx) {
+  const selectionold = document.querySelector(".drinking .border");
+  console.log(produtosx);
+  if (selectionold !== null) {
+    selectionold.classList.remove("border");
+    sum = sum - 1;
+  }
+  const nselect = document.querySelector(produtosx);
+  console.log(nselect);
+  nselect.classList.add("border");
+  sum = sum + 1;
+}
+
+function selectionsweet(produtosx) {
+    const selectionold = document.querySelector(".sweet .border");
+    console.log(produtosx);
+    if (selectionold !== null) {
+      selectionold.classList.remove("border");
+      sum = sum - 1;
+    }
+    const nselect = document.querySelector(produtosx);
+    console.log(nselect);
+    nselect.classList.add("border");
+    sum = sum + 1;
+  }
+  
